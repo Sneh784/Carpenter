addae33138f9734384b028f23cf99f1565fc6401
Sneha <shindedsneha2003@gmail.com>
2025-02-15 17:16:00 +0530

    responsive

--- src/components/datafile/ContactData.jsx
+++ src/components/datafile/ContactData.jsx
@@ -2,8 +2,8 @@ const ContactData = [
     {
       title: 'Get Social',
       // para: 'Contact Info',
-      Mail_ID: 'hr@pentasoftwarereconsultancy.com',
-      Contact_Us: '+91 7066541234|83298294',
+      Mail_ID: 'hr@Dev.com',
+      Contact_Us: '+91 7083298294',
       Adress: 'sr No 78/60, PI.No 1,Wing A, Office 26A, Sai Vision,, Near Govind Garden,PimPle saudagar, Pune - 411027',
     }
   ];
